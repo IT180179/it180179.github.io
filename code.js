7c546f1f455b3f82f063be91c8132cdf26963216
IT180179 <annika.cagitzz@gmail.com>
2020-03-03 14:47:50 +0100

    Initial commit

--- code.js
+++ code.js
@@ -5,5 +5,6 @@
     document.getElementById("body").style.padding ="5%";
     document.getElementById("body").style.fontSize ="160%";
     document.getElementById("body").style.borderRadius ="23px";
+    document.getElementById("body").style.border ="1px solid black";
     
  }
