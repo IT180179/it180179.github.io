4e10e451ee9803a429dc5a591df214743cdcdfec
IT180179 <annika.cagitzz@gmail.com>
2020-03-03 14:34:33 +0100

    Initial commit

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
  function myFunction() {
 
-    document.getElementById("body").style.backgroundColor = "lightseagreen";
+    document.getElementById("body").style.backgroundColor = "lightsteelblue";
     document.getElementById("body").style.fontFamily = "Aria";
     document.getElementById("body").style.padding ="5%";
     document.getElementById("body").style.fontSize ="160%";
