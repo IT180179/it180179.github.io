892edd39b96859578ffbeba719773993b425d5e9
IT180179 <annika.cagitzz@gmail.com>
2020-03-03 14:08:57 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,8 @@
+ function myFunction() {
+    document.getElementById("all").style.backgroundColor = "lightseagreen";
+    document.getElementById("all").style.color = "black";
+    document.getElementById("all").style.fontFamily = "Aria";
+    document.getElementById("all").style.padding ="5%";
+    document.getElementById("all").style.fontSize ="160%";
+    document.getElementById("all").style.borderRadius ="23px";
+ }
